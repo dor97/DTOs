35f30ad121b494d1a728f7d99cf8e065d7b0b472
Dor <dorraba100@gmail.com>
2023-10-10 23:49:54 +0300

    new commit

---
+++ src/DTO/DTOThreadPoolSize.java
@@ -0,0 +1,13 @@
+package DTO;
+
+public class DTOThreadPoolSize {
+    private Integer size;
+
+    public Integer getSize(){
+        return size;
+    }
+
+    public void setSize(Integer size){
+        this.size = size;
+    }
+}
