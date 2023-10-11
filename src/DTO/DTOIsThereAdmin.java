51d9bf553ddcd0d39c6ee3309e15d9f8af84ffd3
Dor <dorraba100@gmail.com>
2023-10-11 22:21:42 +0300

    new commit

---
+++ src/DTO/DTOIsThereAdmin.java
@@ -0,0 +1,13 @@
+package DTO;
+
+public class DTOIsThereAdmin {
+    private Boolean AdminExist;
+
+    public Boolean getAdminExist() {
+        return AdminExist;
+    }
+
+    public void setAdminExist(Boolean adminExist) {
+        AdminExist = adminExist;
+    }
+}
